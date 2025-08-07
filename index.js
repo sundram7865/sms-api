a00bd8c41814c1c7b1e2ade82dc5671a8606d3e1
sundram <ram322222@gmail.com>
2025-08-07 10:55:23 +0530

    created Schoolroutes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 import './config/db.js';
+import schoolRoutes from './routes/schoolRoutes.js';
 dotenv.config();
 const app = express();
 
