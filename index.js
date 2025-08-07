eef3a4c1ef7755bdf05889d799cca67827eb937a
sundram <ram322222@gmail.com>
2025-08-07 08:22:29 +0530

    started basic backend server

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.json({
+        message:"Welcome to School Management System API"
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
